b44b605cd74a3853521ddb09306e5ee5bcc840c5
XoyonSantiago <xoyonsantiago@gmail.com>
2019-09-23 15:48:42 -0600

    proyecto de tweet con componentes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Tweet() {
+    return (
+        <div className="tweet">
+            <Avatar />
+            <div className="content">
+                <NameWithHandle /><Time/>
+                <Message />
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetWeetButton/>
+                    <LikeButton />
+                    <MoreOptionsButton/>
+                </div>
+            </div> 
+        </div>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Avatar() {
+    return (
+        <img
+            src="https://www.gravatar.com/avatar/nothing"
+            className="avatar"
+            alt="avatar"
+        />
+    );
+}
+
+function Message() {
+    return (
+        <div className="message">
+            This is less than 140 characters.
+        </div>
+    );
+}
+
+function NameWithHandle() {
+    return (
+        <span className="name-with-handle">
+            <span className="name">Santiago</span>
+            <span className="handle">@tu tweet</span>
+        </span>
+    );
+}
+
+// otro tipos de funciones guardandolas en constante
+const Time = () => (
+    <span className="time">3h ago</span>
+);
+
+const ReplyButton = () => (
+    <i className="fa fa-reply reply-button"/>
+);
+
+const RetWeetButton = () => (
+    <i className="fa fa-retweet retweet-button"/>
+);
+
+const LikeButton = () => {
+    return (
+        <i className="fa fa-heart like-button" />
+    );
+}
+
+// And if it's really simple,
+// you can even write it on one line:
+// const hi = () => <span>holisss</span>;
+
+const MoreOptionsButton = () => (
+    <i className="fa fa-ellipsis-h more-options-button" />
+);
+
+ReactDOM.render(
+    <Tweet />, // componentes de funcio sin estado o componentes funcional
+    document.querySelector('#root')
+);
